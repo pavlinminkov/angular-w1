a1ae2d42cabe6544ba0349247b7115b3624e08af
pavlinminkov <pmminkov01@gmail.com>
2024-04-25 19:54:34 +0300

    Incorporate toggle-paragraph component in the app component

--- hello-world-2101321031/src/app/app.component.ts
+++ hello-world-2101321031/src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToggleParagraphComponent } from './toggle-paragraph/toggle-paragraph.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, ToggleParagraphComponent],
 })
 export class AppComponent {
-  public personalInformation = "Павлин Минков, Студент Софтуерно Инженерство 3ти курс";
-  public hobies = "Гледане на филми, Баскетбол";
-  public favoriteCreatorLink = "https://www.youtube.com/@TomScottGo";
-  public personalChannelDescription = "Код и кафе - кратки клипове съдържащи последните новини в програмирането всяка сутрин";
-  public evaluation = 0.10;
+  public personalInformation =
+    'Павлин Минков, Студент Софтуерно Инженерство 3ти курс';
+  public hobies = 'Гледане на филми, Баскетбол';
+  public favoriteCreatorLink = 'https://www.youtube.com/@TomScottGo';
+  public personalChannelDescription =
+    'Код и кафе - кратки клипове съдържащи последните новини в програмирането всяка сутрин';
+  public evaluation = "Youtube Adsense, Sponsors";
 }
